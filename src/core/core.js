f74cd05fabb4c8909cef94543a8c743f8c8c44e8
Yngve Svalestuen <yngve@outlook.com>
2015-10-14 13:11:15 +0200

    Scoped generate360view variables to support multiple 360 view elements on same page

--- src/core/core.js
+++ src/core/core.js
@@ -1,29 +1,29 @@
 import getHandler from '../handlers/handler';
 
-let current = 0, images;
-
-function incrementCurrentImage(i) {
-    current += i;
-    if (current === images.length) {
-        current = 0;
-    } else if (current < 0) {
-        current = images.length - 1;
-    }
-}
-
-function hideImages() {
-    for (var i = 1, max = images.length; i < max; i++) {
-        images[i].style.display = 'none';
-    }
-}
-
-function toggleImages(i) {
-    images[current].style.display = 'none';
-    incrementCurrentImage(i);
-    images[current].style.display = 'block';
-}
-
 export default function generate360view(containerId) {
+    let current = 0, images;
+
+    function incrementCurrentImage(i) {
+        current += i;
+        if (current === images.length) {
+            current = 0;
+        } else if (current < 0) {
+            current = images.length - 1;
+        }
+    }
+
+    function hideImages() {
+        for (var i = 1, max = images.length; i < max; i++) {
+            images[i].style.display = 'none';
+        }
+    }
+
+    function toggleImages(i) {
+        images[current].style.display = 'none';
+        incrementCurrentImage(i);
+        images[current].style.display = 'block';
+    }
+
     const container = document.getElementById(containerId);
     const handler = getHandler(container);
     images = document.querySelectorAll("#" + containerId + " img");
